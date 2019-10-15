5ae7f4ab453ea2bbf38e73bc31d70579e9c9fe4f
James Johnson <james.johnson@infuse.it>
2019-10-15 15:55:02 +0100

    Add test names

--- findtests.ps1
+++ findtests.ps1
@@ -1,6 +1,10 @@
-$testIds = & "$env:progDir\FindTests.exe" -s $env:Server -p $env:Project -u $env:FindStatus -f id
+$tests = & "$env:progDir\FindTests.exe" -s $env:Server -p $env:Project -u $env:FindStatus
 $i = 0
 $matrix = @{}
-for ($i; $i -lt ($testIds.Length); $i +=1) { $matrix.add("Test$($i+1)", @{ TestId = $testIds[$i] }) }
+for ($i; $i -lt ($tests.Length); $i +=1)
+{
+  $test = $tests[$i] | ConvertFrom-Json
+  $matrix.add($test.Name, @{ TestId = $test.Id })
+}
 $jsonMatrix = $matrix | ConvertTo-Json -Compress
 Write-Output "##vso[task.setvariable variable=TestId;isOutput=true]$jsonMatrix"
